366215d6fad33ac81fc44ac4cae8cfc5a33eb970
Igor Gitman <igitman@nvidia.com>
2024-12-27 09:49:18 -0800

    Fix check contamination issue
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/pipeline/check_contamination.py
+++ nemo_skills/pipeline/check_contamination.py
@@ -121,6 +121,8 @@ def check_contamination(
             "server_port": server_port,
         }
         extra_arguments += f" ++server.server_type={server_type} "
+        extra_arguments += f" ++server.host=localhost "
+        extra_arguments += f" ++server.port={server_port} "
     else:  # model is hosted elsewhere
         server_config = None
         extra_arguments += (
