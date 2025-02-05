d29b41017a494ca83e95681e2a58c8b1c23a33d9
Somshubra Majumdar <titu1994@gmail.com>
2025-02-05 11:01:25 -0800

    Improve support for OpenRLHF SFT (#351)
    Signed-off-by: smajumdar <titu1994@gmail.com>
    Signed-off-by: Igor Gitman <igitman@nvidia.com>
    Co-authored-by: Igor Gitman <igitman@nvidia.com>

--- nemo_skills/pipeline/openrlhf/sft.py
+++ nemo_skills/pipeline/openrlhf/sft.py
@@ -42,7 +42,7 @@ class TrainingParams:
     expname: str
     disable_wandb: bool
     wandb_project: str
-    timeout: str  # TODO: add proper support
+    timeout: str
     extra_arguments: str = ""
     logging_params: str = ""
 
@@ -87,6 +87,7 @@ def format_train_args(cluster_config, params: TrainingParams):
         f" --save_steps -1 "
         f" --max_samples 500000 "
         f" --max_epochs 1 "
+        f" --max_time_per_run {params.timeout} "
     )
     return cmd
 
@@ -143,7 +144,7 @@ def get_cmd(cluster_config, params: TrainingParams):
         f"cd /nemo_run/code && "
         f"echo 'Starting SFT' && "
         f'echo "Torch run cmd: {torchrun_cmd}" && '
-        f"{torchrun_cmd} -m openrlhf.cli.train_sft "
+        f"{torchrun_cmd} -m nemo_skills.training.openrlhf.sft_script "
         f"  {format_train_args(cluster_config, params)} "
         f"  {format_data_args(cluster_config, params)} "
         f"  {get_common_arg_overrides(cluster_config, params)} "
---
+++ nemo_skills/training/openrlhf/sft_script.py
@@ -0,0 +1,358 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copyright (c) 2025, Jian Hu.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# File is modified from https://github.com/OpenRLHF/OpenRLHF/blob/main/openrlhf/cli/train_sft.py
+
+import re
+import time
+import argparse
+import math
+import os
+from datetime import datetime
+
+from transformers.trainer import get_scheduler
+
+from openrlhf.datasets import SFTDataset
+from openrlhf.models import Actor
+from openrlhf.trainer import SFTTrainer
+from openrlhf.utils import blending_datasets, get_strategy, get_tokenizer
+
+
+class MaxTimeManager:
+    def __init__(self, max_time: str):
+        """
+        Stops training after time interval has been reached.
+        Args:
+            max_time (str): The max time to run in HH:MM:SS format.
+
+        Raises:
+            ValueError: If the time interval format is invalid.
+        """
+        self.save_interval = self._parse_time_interval(max_time)
+        self.max_time_reached = False
+        self.start_time = time.time()
+
+    def _parse_time_interval(self, interval_str):
+        match = re.fullmatch(r"(\d+):([0-5]?\d):([0-5]?\d)", interval_str)
+        if not match:
+            raise ValueError(
+                f"Invalid time interval format: '{interval_str}'. Use HH:MM:SS format."
+            )
+        hours, minutes, seconds = map(int, match.groups())
+        return hours * 3600 + minutes * 60 + seconds
+
+    def check(self):
+        current_time = time.time()
+        if (current_time - self.start_time) >= self.save_interval:
+            print(f"\n\n>>> Max time has been reached. Signalling to save a checkpoint.\n\n")
+            self.max_time_reached = True
+        return self.max_time_reached
+
+
+class CustomSFTTrainer(SFTTrainer):
+
+    def __init__(self, *args, max_time_per_run=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.max_time_per_run = max_time_per_run
+
+        if self.max_time_per_run:
+            self.max_time_manager = MaxTimeManager(self.max_time_per_run)
+        else:
+            self.max_time_manager = None
+
+    def save_logs_and_checkpoints(self, args, global_step, step_bar, logs_dict={}, client_states={}):
+        if self.max_time_manager is not None:
+            check = self.max_time_manager.check()
+            if check:
+                # We will force logging, evaluation and checkpointing to occur immediately
+                # by forcing the value of these step counter equal to global_step
+                args.logging_steps = global_step if args.logging_steps > 0 else args.logging_steps
+                args.eval_steps = global_step if args.eval_steps > 0 else args.eval_steps
+                args.save_steps = global_step if args.save_steps > 0 else args.save_steps
+
+                # Call super() to save the logs and checkpoints
+                super().save_logs_and_checkpoints(args, global_step, step_bar, logs_dict, client_states)
+
+                # Exit the program early
+                exit(0)
+
+        else:
+            super().save_logs_and_checkpoints(args, global_step, step_bar, logs_dict, client_states)
+
+
+def train(args):
+    # configure strategy
+    strategy = get_strategy(args)
+    strategy.setup_distributed()
+
+    # configure model
+    # load huggingface model
+    model = Actor(
+        args.pretrain,
+        use_flash_attention_2=args.flash_attn,
+        bf16=args.bf16,
+        load_in_4bit=args.load_in_4bit,
+        lora_rank=args.lora_rank,
+        lora_alpha=args.lora_alpha,
+        target_modules=args.target_modules,
+        lora_dropout=args.lora_dropout,
+        ds_config=strategy.get_ds_train_config(is_actor=True),
+        packing_samples=args.packing_samples,
+    )
+    # configure tokenizer
+    tokenizer = get_tokenizer(args.pretrain, model.model, "right", strategy, use_fast=not args.disable_fast_tokenizer)
+    strategy.print(model)
+
+    # gradient_checkpointing
+    if args.gradient_checkpointing:
+        model.gradient_checkpointing_enable(
+            gradient_checkpointing_kwargs={"use_reentrant": args.gradient_checkpointing_use_reentrant}
+        )
+
+    # configure optimizer
+    optim = strategy.create_optimizer(model, lr=args.learning_rate, betas=args.adam_betas, weight_decay=args.l2)
+
+    # prepare for data and dataset
+    train_data, eval_data = blending_datasets(
+        args.dataset,
+        args.dataset_probs,
+        strategy,
+        args.seed,
+        max_count=args.max_samples,
+        train_split=args.train_split,
+        eval_split=args.eval_split,
+    )
+    train_data = train_data.select(range(min(args.max_samples, len(train_data))))
+    if args.eval_steps > 0:
+        eval_data = eval_data.select(range(min(args.max_samples, len(eval_data))))
+    else:
+        eval_data = train_data.select(range(min(args.max_samples, max(1, int(len(train_data) * 0.03)))))
+    train_dataset = SFTDataset(
+        train_data,
+        tokenizer,
+        args.max_len,
+        strategy,
+        pretrain_mode=args.pretrain_mode,
+        input_template=args.input_template,
+        multiple_of=args.ring_attn_size,
+        multiturn=args.multiturn,
+    )
+    eval_dataset = SFTDataset(
+        eval_data,
+        tokenizer,
+        args.max_len,
+        strategy,
+        pretrain_mode=args.pretrain_mode,
+        input_template=args.input_template,
+        multiple_of=args.ring_attn_size,
+        multiturn=args.multiturn,
+    )
+
+    # prepare dataloader
+    train_dataloader = strategy.setup_dataloader(
+        train_dataset,
+        args.micro_train_batch_size,
+        True,
+        True,
+        train_dataset.packing_collate_fn if args.packing_samples else train_dataset.collate_fn,
+    )
+    eval_dataloader = strategy.setup_dataloader(
+        eval_dataset,
+        args.micro_train_batch_size,
+        True,
+        False,
+        eval_dataset.packing_collate_fn if args.packing_samples else eval_dataset.collate_fn,
+    )
+
+    # scheduler
+    num_update_steps_per_epoch = len(train_dataset) // args.train_batch_size
+    max_steps = math.ceil(args.max_epochs * num_update_steps_per_epoch)
+
+    scheduler = get_scheduler(
+        args.lr_scheduler,
+        optim,
+        num_warmup_steps=math.ceil(max_steps * args.lr_warmup_ratio),
+        num_training_steps=max_steps,
+        scheduler_specific_kwargs={"min_lr": args.learning_rate * 0.1},
+    )
+
+    # prepare models
+    (model, optim, scheduler) = strategy.prepare((model, optim, scheduler))
+
+    # load checkpoint
+    consumed_samples = 0
+    if args.load_checkpoint and os.path.exists(args.ckpt_path):
+        if len(os.listdir(args.ckpt_path)) > 0:
+            _, states = strategy.load_ckpt(model.model, args.ckpt_path)
+            consumed_samples = states["consumed_samples"]
+            strategy.print(f"Loaded the checkpoint: {args.ckpt_path}, consumed_samples: {consumed_samples}")
+
+    os.makedirs(args.save_path, exist_ok=True)
+
+    # configure Trainer
+    trainer = CustomSFTTrainer(
+        model=model,
+        strategy=strategy,
+        optim=optim,
+        train_dataloader=train_dataloader,
+        eval_dataloader=eval_dataloader,
+        scheduler=scheduler,
+        max_norm=args.max_norm,
+        pretrain_mode=args.pretrain_mode,
+        batch_size=args.train_batch_size,
+        max_epochs=args.max_epochs,
+        tokenizer=tokenizer,
+        save_hf_ckpt=args.save_hf_ckpt,
+        disable_ds_ckpt=args.disable_ds_ckpt,
+        max_time_per_run=args.max_time_per_run,
+    )
+
+    trainer.fit(args, consumed_samples, num_update_steps_per_epoch)
+
+    # save model checkpoint after fitting on only rank0
+    strategy.save_model(model, tokenizer, args.save_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Checkpoint
+    parser.add_argument("--save_path", type=str, default="./ckpt")
+    parser.add_argument("--save_steps", type=int, default=-1)
+    parser.add_argument("--save_hf_ckpt", action="store_true", default=False)
+    parser.add_argument("--disable_ds_ckpt", action="store_true", default=False)
+    parser.add_argument("--logging_steps", type=int, default=1)
+    parser.add_argument("--eval_steps", type=int, default=-1)
+    parser.add_argument("--ckpt_path", type=str, default="./ckpt/checkpoints_sft")
+    parser.add_argument("--max_ckpt_num", type=int, default=3)
+    parser.add_argument("--max_ckpt_mem", type=int, default=1e8)
+    parser.add_argument("--load_checkpoint", action="store_true", default=False)
+
+    # DeepSpeed
+    parser.add_argument("--micro_train_batch_size", type=int, default=8, help="batch size per GPU")
+    parser.add_argument("--train_batch_size", type=int, default=128, help="Global training batch size")
+    parser.add_argument("--max_norm", type=float, default=1.0, help="Gradient clipping")
+    parser.add_argument("--gradient_checkpointing", action="store_true", default=False)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--local_rank", type=int, default=-1, help="local_rank for deepspeed")
+    parser.add_argument("--zero_stage", type=int, default=2, help="DeepSpeed ZeRO stage")
+    parser.add_argument("--bf16", action="store_true", default=False, help="Enable bfloat16")
+    parser.add_argument("--zpg", type=int, default=1, help="ZeRO++ max partition size")
+    parser.add_argument("--adam_offload", action="store_true", default=False, help="Offload Adam Optimizer")
+    parser.add_argument("--flash_attn", action="store_true", default=False, help="Enable FlashAttention2")
+    parser.add_argument("--grad_accum_dtype", type=str, default=None, help="Adam grad accum data type")
+    parser.add_argument("--overlap_comm", action="store_true", default=False)
+    parser.add_argument("--gradient_checkpointing_use_reentrant", action="store_true", default=False)
+    parser.add_argument("--disable_fast_tokenizer", action="store_true", default=False)
+
+    # SFT
+    parser.add_argument("--max_epochs", type=int, default=2)
+    parser.add_argument("--aux_loss_coef", type=float, default=0, help="MoE balancing loss")
+    parser.add_argument("--pretrain", type=str, default=None)
+    parser.add_argument("--learning_rate", type=float, default=5e-6)
+    parser.add_argument("--lr_warmup_ratio", type=float, default=0.03)
+    parser.add_argument("--pretrain_mode", action="store_true", default=False, help="Use pretrain loss")
+    parser.add_argument("--lr_scheduler", type=str, default="cosine_with_min_lr")
+    parser.add_argument("--l2", type=float, default=0, help="weight decay loss")
+    parser.add_argument("--adam_betas", type=float, nargs=2, default=(0.9, 0.95), help="Betas for Adam optimizer")
+
+    # ring-attention
+    parser.add_argument("--ring_attn_size", type=int, default=1, help="Ring attention group size")
+    parser.add_argument(
+        "--ring_head_stride",
+        type=int,
+        default=1,
+        help="the number of heads to do ring attention each time. "
+        "It should be a divisor of the number of heads. "
+        "A larger value may results in faster training but will consume more memory.",
+    )
+
+    # LoRA
+    parser.add_argument("--load_in_4bit", action="store_true", default=False)
+    parser.add_argument("--lora_rank", type=int, default=0)
+    parser.add_argument("--lora_alpha", type=int, default=16)
+    parser.add_argument("--target_modules", type=str, nargs="*", default="all-linear")
+    parser.add_argument("--lora_dropout", type=float, default=0)
+
+    # packing SFT samples without CrossAttention
+    parser.add_argument("--packing_samples", action="store_true", default=False)
+
+    # custom dataset
+    parser.add_argument("--dataset", type=str, default=None)
+    parser.add_argument("--dataset_probs", type=str, default="1.0", help="sampling probs for datasets")
+    parser.add_argument("--train_split", type=str, default="train", help="train split of the HF dataset")
+    parser.add_argument("--eval_split", type=str, default="test", help="test split of the dataset")
+    parser.add_argument("--multiturn", action="store_true", default=False, help="Use compacted multiturn dataset")
+
+    parser.add_argument("--input_key", type=str, default="input", help="JSON dataset key")
+    parser.add_argument("--output_key", type=str, default=None, help="JSON dataset key")
+    parser.add_argument("--input_template", type=str, default="User: {}\nAssistant: ")
+    parser.add_argument(
+        "--apply_chat_template", action="store_true", default=False, help="Use HF tokenizer chat template"
+    )
+    parser.add_argument("--tokenizer_chat_template", type=str, default=None)
+    parser.add_argument("--max_samples", type=int, default=1e8, help="Max number of samples")
+    parser.add_argument("--max_len", type=int, default=2048, help="Max tokens for the samples")
+    parser.add_argument("--max_time_per_run", type=str, default=None, help="Max time per run in HH:MM:SS format")
+
+    # wandb parameters
+    parser.add_argument("--use_wandb", type=str, default=None)
+    parser.add_argument("--wandb_org", type=str, default=None)
+    parser.add_argument("--wandb_group", type=str, default=None)
+    parser.add_argument("--wandb_project", type=str, default="openrlhf_train_sft")
+    parser.add_argument(
+        "--wandb_run_name",
+        type=str,
+        default="sft_%s" % datetime.now().strftime("%m%dT%H:%M"),
+    )
+
+    # TensorBoard parameters
+    parser.add_argument("--use_tensorboard", type=str, default=None, help="TensorBoard logging path")
+
+    args = parser.parse_args()
+
+    if args.multiturn:
+        assert args.apply_chat_template, "apply_chat_template must be enabled when using multiturn format"
+
+    if args.input_template and "{}" not in args.input_template:
+        print("[Warning] {} not in args.input_template, set to None")
+        args.input_template = None
+
+    if args.input_template and "\\n" in args.input_template:
+        print(
+            "[Warning] input_template contains \\n chracters instead of newline. "
+            "You likely want to pass $'\\n' in Bash or \"`n\" in PowerShell."
+        )
+
+    if args.packing_samples and not args.flash_attn:
+        print("[Warning] Please --flash_attn to accelerate when --packing_samples is enabled.")
+        args.flash_attn = True
+
+    if args.ring_attn_size > 1:
+        assert args.packing_samples, "packing_samples must be enabled when using ring attention"
+
+    train(args)
