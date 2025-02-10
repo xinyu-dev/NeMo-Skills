88140f750d2c4841047461c7c5b07cdf6c6d7905
Shubham Toshniwal <shtoshni@gmail.com>
2025-02-10 16:26:59 -0500

    Shtoshni/aime 2025 (#374)

---
+++ nemo_skills/dataset/aime25/prepare.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import shutil
+from pathlib import Path
+
+if __name__ == "__main__":
+    data_dir = Path(__file__).absolute().parent
+    data_dir.mkdir(exist_ok=True)
+
+    # TODO: Only part I has been released so far. Add part II later.
+    original_file = str(data_dir / "test-1.txt")
+    output_file = str(data_dir / "test.jsonl")
+    shutil.copy(original_file, output_file)
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -18,6 +18,7 @@ from pathlib import Path
 
 # tuple of dataset name, available splits and prepared sft files
 DATASETS = [
+    ('aime25', ['test']),
     ('aime24', ['test']),
     ('amc23', ['test']),
     ('omni-math', ['test']),
