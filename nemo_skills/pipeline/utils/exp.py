9ea9677e532fe277fafcc6474e4ce151091ac10e
Sean Naren <snarenthiran@nvidia.com>
2025-08-16 16:32:49 +0100

    allow overlapping sandbox with run_cmd (#680)
    Signed-off-by: SeanNaren <snarenthiran@nvidia.com>

--- nemo_skills/pipeline/utils/exp.py
+++ nemo_skills/pipeline/utils/exp.py
@@ -471,7 +471,7 @@ def add_task(
                         heterogeneous=heterogeneous,
                         het_group=het_group,
                         total_het_groups=total_het_groups,
-                        overlap=server_config is not None,
+                        overlap=(server_config is not None) or with_sandbox,
                         with_ray=with_ray,
                     )
                 )
@@ -509,7 +509,7 @@ def add_task(
                 heterogeneous=heterogeneous,
                 het_group=het_group,
                 total_het_groups=total_het_groups,
-                overlap=server_config is not None,
+                overlap=True,
                 with_ray=with_ray,
             )
             executors.append(sandbox_executor)
